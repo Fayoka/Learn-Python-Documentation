3d4097cabc9511b1af1589b55ed5e5f5d33dd13b
Fayoka <60096230+Fayoka@users.noreply.github.com>
2024-10-25 15:29:24 +0200

    deleted test files

--- all-testing.py
+++
@@ -1,7 +0,0 @@
-def testing():
-    myList = [1, 2]
-    # Using all() to check if all items are truthy (non-zero)
-    return all(bool(listItem) for listItem in myList)
-
-# Print the result of testing()
-print(testing())  # This will print True if all elements are non-zero
